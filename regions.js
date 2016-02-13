1622f453e9e3ba134d8ccb11b4a64025434bba7d
Andrés Estévez <aecostas@gmail.com>
2016-02-13 20:05:05 +0100

    Get and process data within a promise

--- regions.js
+++ regions.js
@@ -3,25 +3,48 @@
 var request = require('request');
 var cheerio = require('cheerio');
 
-request({
-    uri: "http://www.morningstar.es/es/funds/snapshot/snapshot.aspx?id=F00000MKIO&tab=3",
-}, function(error, response, body) {
-    var $ = cheerio.load(body);
-    var regions=[]
-    $(".portfolioRegionalBreakdownTable tr").slice(3).each(function() {
-	var children = $(this).children();
-	var region;
-	try {
-	    region = {
-		"region": children[0].children[0].data,
-		"percentage": parseFloat(children[1].children[0].data),
-	    }
-	    regions.push(region);	    
-	} catch(err) {
-	    console.warn
-	}
+var funds = [];
+var promiseFunds = [];
 
-    });
+funds.push({url:"http://www.morningstar.es/es/funds/snapshot/snapshot.aspx?id=F00000MKIO&tab=3"})
+funds.push({url:"http://www.morningstar.es/es/funds/snapshot/snapshot.aspx?id=F00000HLBC&tab=3"})
+funds.push({url:"http://www.morningstar.es/es/funds/snapshot/snapshot.aspx?id=F0GBR04SLW&tab=3"})
+funds.push({url:"http://www.morningstar.es/es/funds/snapshot/snapshot.aspx?id=F00000HLB6&tab=3"})
+funds.push({url:"http://www.morningstar.es/es/funds/snapshot/snapshot.aspx?id=F0GBR04G6K&tab=3"})
+funds.push({url:"http://www.morningstar.es/es/funds/snapshot/snapshot.aspx?id=F000001AAQ&tab=3"})
 
-    console.warn(regions);
-});
+function parseFundRegions(url) {
+    return new Promise(function(resolve, reject) {
+	request({
+	    uri: url,
+	}, function(error, response, body) {
+	    var $ = cheerio.load(body);
+	    var regions=[]
+	    $(".portfolioRegionalBreakdownTable tr").slice(3).each(function() {
+		var children = $(this).children();
+		var region;
+		try {
+		    region = {
+			"region": children[0].children[0].data,
+			"percentage": parseFloat(children[1].children[0].data.replace(',','.')),
+		    }
+		    regions.push(region);
+		} catch(err) {
+		    console.error(err)
+		}
+
+	    });
+	    resolve(regions);
+	})
+    })// Promise
+}
+
+funds.forEach(function(fund) {
+    promiseFunds.push(parseFundRegions(fund.url));
+})
+
+Promise.all(promiseFunds).then(function(values) {
+    console.warn(values);
+})
+
+
