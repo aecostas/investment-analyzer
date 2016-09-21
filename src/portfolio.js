abe3e47c121a0156740cd658f902a111335d184a
Andrés Estévez <aecostas@gmail.com>
2016-09-22 00:55:24 +0200

    Ability to indicate the investment per fund basis

--- src/portfolio.js
+++ src/portfolio.js
@@ -14,6 +14,7 @@ class Portfolio {
 	this.token = 'aaaaa';
 	this.funds = {};
 	this.stats = {};
+	this.total_investment = 0;
     }
 
     _calculate() {
@@ -23,22 +24,30 @@ class Portfolio {
 	results['sectors'] = {};
 	let keys = Object.keys(this.funds);
 
+
+	// update total investment
+	let temp_total_investment = 0;
+ 	keys.forEach(function(key) {
+	    temp_total_investment += self.funds[key].investment;
+	});
+	this.total_investment = temp_total_investment;
+	
  	keys.forEach(function(key) {
 	    let fund = self.funds[key];
-	    fund.regions.forEach(function(data) {
+	    fund.data.regions.forEach(function(data) {
 		if (results.regions[data.region] === undefined) {
 		    results.regions[data.region] = {};
-		    results.regions[data.region].percentage = data.percentage / keys.length;
+		    results.regions[data.region].percentage = data.percentage * fund.investment/self.total_investment;
 		} else {
-		    results.regions[data.region].percentage += data.percentage / keys.length;
+		    results.regions[data.region].percentage += data.percentage * fund.investment/self.total_investment;
 		}
 	    });
 
-	    fund.sectors.forEach(function(data) {
+	    fund.data.sectors.forEach(function(data) {
 		if (results.sectors[data.sector] === undefined) {
-		    results.sectors[data.sector] = data.percentage / keys.length;
+		    results.sectors[data.sector] = data.percentage * fund.investment/self.total_investment;
 		} else {
-		    results.sectors[data.sector] += data.percentage / keys.length;
+		    results.sectors[data.sector] += data.percentage * fund.investment/self.total_investment;
 		}
 	    });
 	});
@@ -63,9 +72,11 @@ class Portfolio {
      * updated automatically
      *
      * @param {String} isin - ISIN of the fund to add
+     * @param {Number} investment - Amount of money invested
+     *                          in this fund
      * @return {Promise}
      */
-    add(isin) {
+    add(isin, investment) {
 	var self = this;
 	return new Promise(function(resolve, reject) {
 
@@ -77,16 +88,20 @@ class Portfolio {
 		}
 		
 		var collection = db.collection('funds');
-		
+
 		collection.findOne({isin:isin}, function(err, docs) {
 		    if (err) {
 			reject();
 		    }
-		    self.funds[isin] = docs;
+		    self.funds[isin] = {};
+		    self.funds[isin].data = docs;
+		    self.funds[isin].investment = investment;
+		    console.dir(self.funds[isin].data.sectors);
 		    db.close();
 		    self.stats = self._calculate();
 		    resolve();
 		});
+		
 	    });
 	});
     }// add
