d6080b232637201798dab14175628922c228f093
Andrés Estévez <aecostas@gmail.com>
2016-09-30 16:58:23 +0200

    Consider database error on getting all funds

--- src/fund-manager.js
+++ src/fund-manager.js
@@ -12,14 +12,19 @@ app.post('/fund', function(req, res) {
 app.get('/fund', function (req, res) {
     // TODO: query string to smart search
     // fund that best match a given criteria
-    database.getListOfFunds().then(function(docs){
-	res.status(200).send(docs);
-    });
+    database.getListOfFunds()
+	.then(function(docs){
+	    res.status(200).send(docs);
+	})
+	.catch(function(err) {
+	    console.warn('Database error:', err);
+	    res.status(500).send();
+	});
 });
 
 app.get('/fund/:funid', function (req, res) {
     database.getFund(req.params.funid)
-	.then(function(docs){
+	.then(function(docs) {
 	    if (docs != null) {
 		res.status(200).send(docs);
 	    } else {
